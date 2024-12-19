1645654dd4023f8d75977b8404fd18bad1d3f20d
sankar <Sankar Boro>
2024-12-19 21:26:21 +0530

    loony.conf nginx setup

--- linux/firstinstall.sh
+++ linux/firstinstall.sh
@@ -34,8 +34,20 @@ done
 # Install programming languages and package managers
 # Node.js and npm
 if ! is_installed "nodejs"; then
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    sudo apt install -y nodejs
+    # installs nvm (Node Version Manager)
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+    # download and install Node.js (you may need to restart the terminal)
+    nvm install 18
+
+    # verifies the right Node.js version is in the environment
+    node -v # should print `v18.20.5`
+
+    # verifies the right npm version is in the environment
+    npm -v # should print `10.8.2`
+
+    # verifies the right nvm version is in the environment
+    nvm -v # should print `0.40.1`
 else
     echo "Node.js is already installed. Skipping."
 fi
